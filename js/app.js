7ba240b6a93282cfbff044b72866892db054f285
João M <joaom182@hotmail.com>
2014-09-17 21:50:41 -0300

    Loader

--- js/app.js
+++ js/app.js
@@ -13,16 +13,21 @@ app.controller("MainController", ["$scope", "$http", function($scope, $http){
     en: "Write your text here that want to generate the audio link"
   }
 
+  $scope.isLoading = false;
   $scope.audioText = "";
   $scope.generatedLink = "https://goo.gl/";
   $scope.inputPlaceholder = _placeholderText.pt;
   $scope.language = "pt";
 
   $scope.getLink = function(){
+    $scope.isLoading = true;
     $http.post('https://www.googleapis.com/urlshortener/v1/url', {
       longUrl: "https://translate.google.com/translate_tts?ie=UTF-8&q=" + $scope.audioText + "&tl=" + $scope.language + "&total=8&idx=3&textlen=58&client=t&prev=input"
     }).success(function(data, status, headers, config) {
       $scope.generatedLink = data.id;
+      $scope.isLoading = false;
+    }).error(function(){
+      $scope.isLoading = false;
     });
   }
 
