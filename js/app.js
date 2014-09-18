e67beccad4d1c74f42cdec6a6be8ba8b01a83cac
João M <joaom182@hotmail.com>
2014-09-17 21:34:25 -0300

    Languages

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ app.config(['$httpProvider', function($httpProvider) {
 app.controller("MainController", ["$scope", "$http", function($scope, $http){
   $scope.audioText = "";
   $scope.generatedLink = "https://goo.gl/";
+  $scope.language = "pt";
 
   $scope.getLink = function(){
     $http.post('https://www.googleapis.com/urlshortener/v1/url', {
-      longUrl: "https://translate.google.com/translate_tts?ie=UTF-8&q=" + $scope.audioText + "&tl=pt&total=8&idx=3&textlen=58&client=t&prev=input"
+      longUrl: "https://translate.google.com/translate_tts?ie=UTF-8&q=" + $scope.audioText + "&tl=" + $scope.language + "&total=8&idx=3&textlen=58&client=t&prev=input"
     }).success(function(data, status, headers, config) {
       $scope.generatedLink = data.id;
     });
