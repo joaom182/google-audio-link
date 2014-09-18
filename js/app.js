8ccd10ccafd70925808cb5d882bfe66ac1962ab2
João M <joaom182@hotmail.com>
2014-09-17 21:24:02 -0300

    Styles

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module("app", []);
+
+app.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+app.controller("MainController", ["$scope", "$http", function($scope, $http){
+  $scope.audioText = "";
+  $scope.generatedLink = "https://goo.gl/";
+
+  $scope.getLink = function(){
+    $http.post('https://www.googleapis.com/urlshortener/v1/url', {
+      longUrl: "https://translate.google.com/translate_tts?ie=UTF-8&q=" + $scope.audioText + "&tl=pt&total=8&idx=3&textlen=58&client=t&prev=input"
+    }).success(function(data, status, headers, config) {
+      $scope.generatedLink = data.id;
+    });
+  }
+}]);
